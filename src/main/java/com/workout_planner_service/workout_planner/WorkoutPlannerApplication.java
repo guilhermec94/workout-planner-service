0b234d523b0430ad0036f55749f2f3e22bbecf70
Guilherme <gui.carvalho94@gmail.com>
2024-10-09 19:24:17 +0100

    chore: initial project setup with liquibase

---
+++ src/main/java/com/workout_planner_service/workout_planner/WorkoutPlannerApplication.java
@@ -0,0 +1,12 @@
+package com.workout_planner_service.workout_planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(WorkoutPlannerApplication.class, args);
+  }
+}
---
+++ src/test/java/com/workout_planner_service/workout_planner/WorkoutPlannerApplicationTests.java
@@ -0,0 +1,11 @@
+package com.workout_planner_service.workout_planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPlannerApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
